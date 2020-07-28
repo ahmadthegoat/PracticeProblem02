584ab0b1f14949de8abaca3150c4a01129856559
ahmadthegoat <ahcon1@morgan.edu>
2020-07-28 14:50:40 -0400

    finished

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,22 @@
 package com.stayready;
 
+import java.lang.reflect.Array;
+
+import static java.util.Arrays.*;
+
 public class Problem02 {
+    public String countinous(Integer[] numarray){
+        sort(numarray);
+        int length = 1;
+        int count = 0;
+        for(Integer number : numarray){
+            if(count < 4){
+            Integer nextnumber = numarray[count+1];
+            count ++;
+            if(nextnumber-number == (1)){
+                length ++;
+            }}
+        }
+        return "Length of the longest contiguous subarray is " + length;
+    }
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,33 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+
 public class Problem02Test {
+    Problem02 problem02 = new Problem02();
+
+    @Test
+    public void conutinoustest() {
+        Integer[] given = new Integer[]{14, 12, 11, 20};
+        String result = problem02.countinous(given);
+
+
+        String expected = "Length of the longest contiguous subarray is 2";
+
+        Assert.assertEquals(expected, result);
+    }
+
+
+    @Test
+    public void conutinoustest1(){
+        Integer[] given = new Integer[]{170, 169, 168, 1};
+        String result = problem02.countinous(given);
+
+
+        String expected = "Length of the longest contiguous subarray is 3";
+
+        Assert.assertEquals(expected,result);
+    }
 }
